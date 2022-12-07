c13d1da50f1d84110a1963aaa668a9b4e07b584b
sina-hadi <sinahadi01@gmail.com>
2022-12-07 17:49:13 +0330

    db test 1

---
+++ app/src/androidTest/java/com/codinginflow/testproject/data/ShoppingDaoTest.kt
@@ -0,0 +1,49 @@
+package com.codinginflow.testproject.data
+
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.codinginflow.testproject.data.local.ShoppingDao
+import com.codinginflow.testproject.data.local.ShoppingItem
+import com.codinginflow.testproject.data.local.ShoppingItemDatabase
+import com.codinginflow.testproject.getOrAwaitValue
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class ShoppingDaoTest {
+
+    private lateinit var database: ShoppingItemDatabase
+    private lateinit var dao: ShoppingDao
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            ShoppingItemDatabase::class.java
+        ).allowMainThreadQueries().build()
+        dao = database.shoppingDao()
+    }
+
+    @After
+    fun teardown() {
+        database.close()
+    }
+
+    @Test
+    fun testInsertShoppingItem() = runBlocking {
+        val shoppingItem = ShoppingItem("name", 1, 1f, "url", id = 1)
+        dao.insertShoppingItem(shoppingItem)
+
+        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
+
+        assertThat(allShoppingItems).contains(shoppingItem)
+    }
+
+}
